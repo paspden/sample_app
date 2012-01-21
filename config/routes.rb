7e049afab593e528c3a8df2ba8de213faf516cd6
Paul Aspden <paspden@gmail.com>
2012-01-22 10:17:05 +1100

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
+  match '/signup', :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/home',    :to => 'pages#home'
   root :to => 'pages#home'
 
 
